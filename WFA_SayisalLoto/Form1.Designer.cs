d963c7984941bfe40efdb6c554d18f540d6dc325
deveper <semsnt12@gmail.com>
2020-02-28 17:39:41 +0200

    Add project files.

---
+++ WFA_SayisalLoto/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WFA_SayisalLoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(23, 184);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(630, 53);
+            this.metroTile1.TabIndex = 9;
+            this.metroTile1.Text = "Çekiliş Yap";
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // txt6
+            // 
+            this.txt6.Enabled = false;
+            this.txt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt6.Location = new System.Drawing.Point(553, 96);
+            this.txt6.Multiline = true;
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(100, 82);
+            this.txt6.TabIndex = 6;
+            this.txt6.Text = "0";
+            this.txt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt4
+            // 
+            this.txt4.Enabled = false;
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.Location = new System.Drawing.Point(341, 96);
+            this.txt4.Multiline = true;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 82);
+            this.txt4.TabIndex = 7;
+            this.txt4.Text = "0";
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt2
+            // 
+            this.txt2.Enabled = false;
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(129, 96);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 82);
+            this.txt2.TabIndex = 8;
+            this.txt2.Text = "0";
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt5
+            // 
+            this.txt5.Enabled = false;
+            this.txt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt5.Location = new System.Drawing.Point(447, 96);
+            this.txt5.Multiline = true;
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 82);
+            this.txt5.TabIndex = 3;
+            this.txt5.Text = "0";
+            this.txt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt3
+            // 
+            this.txt3.Enabled = false;
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.Location = new System.Drawing.Point(235, 96);
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 82);
+            this.txt3.TabIndex = 4;
+            this.txt3.Text = "0";
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt1
+            // 
+            this.txt1.Enabled = false;
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(23, 96);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 82);
+            this.txt1.TabIndex = 5;
+            this.txt1.Text = "0";
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 261);
+            this.Controls.Add(this.metroTile1);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Sayısal Loto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WFA_SayisalLoto/Form1.cs
@@ -0,0 +1,100 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WFA_SayisalLoto
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+   
+        int toplam = 0;
+        int sayi = 0;
+        List<int> loto = new List<int>();
+        Control[] controls;
+        int textboxNumber = 0;
+        private void metroTile1_Click(object sender, System.EventArgs e)
+        {
+           
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Control txt = controls[textboxNumber];
+
+            if (textboxNumber<6)
+            {
+                //sayi = rnd.Next(1, 49);
+                //txt.Text = sayi.ToString();
+                //toplam++;
+                //if (!loto.Contains(sayi))
+                //{
+                //    loto.Add(sayi);
+                //}
+                //else
+                //{
+                //    toplam--;
+                //}
+                //if (toplam == 10)
+                //{
+                //    timer1.Stop();
+                //    toplam = 0;
+                //}
+
+                if (toplam < 11)
+                {
+                    sayi = rnd.Next(1, 7);
+                    txt.Text = sayi.ToString();
+                    toplam ++;
+                }
+                else if (toplam == 11)
+                {
+                    if (!loto.Contains(sayi))
+                    {
+                        loto.Add(sayi);
+                        txt.Text = sayi.ToString();
+                    }
+                    else
+                    {
+                        toplam--;
+                        
+                    }
+                    textboxNumber++;
+                    toplam = 0;
+                    if (textboxNumber == 6)
+                    {
+                        loto.Sort();
+                        for (int i = 0; i < loto.Count-1; i++)
+                        {
+                            controls[i].Text = loto[i].ToString();
+                        }
+                        timer1.Stop();
+
+                    }
+                }
+            }
+
+            
+
+        }
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            controls = new Control[]
+           {
+                txt1, txt2, txt3, txt4, txt5, txt6
+           };
+        }
+    }
+}
